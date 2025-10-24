ac15b15dcf543cc6cf90f2c725a01de995e2d6c7
Loukmane57 <sawadogoloukmane6511@gmail.com>
2025-10-24 11:45:38 +0000

    message

---
+++ minimum.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  3 10:31:53 2025
+
+@author: LOUKMANE
+"""
+
+a=float(input("donner la valeur de a:"))
+b=float(input("donner la valeur de b:"))
+if(a>=b):
+    min=b
+    max=a
+else:
+    min=a
+    max=b
+print("le maximum de ",a," et de ",b," est :",max)
+print("le minimum de ",a," et de ",b," est:",min)
+          
