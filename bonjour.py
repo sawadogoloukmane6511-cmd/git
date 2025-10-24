048b65d82bfe76aeb7489632392fda619e4e2867
Loukmane57 <sawadogoloukmane6511@gmail.com>
2025-10-24 11:59:41 +0000

    message

---
+++ bonjour.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  5 20:13:28 2025
+
+@author: LOUKMANE
+"""
+
+nom=(input("veuiller saisir un nom:"))
+age=int(input("veuiller saisir votre age:"))
+print("Bonjour",nom,"tu as",age,"ans","Bienvenu à l'université")
